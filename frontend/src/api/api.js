9e631b947cdf5d9e04b51799a1950a0940673fbc
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-01-31 14:52:53 +0530

    url chanmged

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'http://localhost:7000'
+    // baseURL:'http://localhost:7000'
+    baseURL:'https://erp.camet.in/'
     // baseURL:'https://camet.midhunmohan.online/'
 });
